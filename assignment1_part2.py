14489cd7b420ad31c0a0f34c1718be8f09272796
LLBL23 <ltbowenslarson@gmail.com>
2024-01-29 18:13:32 -0500

    commit my changes to Book

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,24 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
+        """
+        Constructor of Book class
+        author = Author of the book
+        title = Title of the book
+        """
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
+        """
+        Display the book's title and author
+        """
+        print(f'"{self.title}, written by {self.author}".')
 
 
 if __name__ == "__main__":
-    pass
+    bk1 = Book('J.K. Rowling', 'Harry Potter and the Goblet of Fire')
+    bk2 = Book('Walter Scott', 'Ivanhoe: Romance')
+
+    bk1.display()
+    bk2.display()
