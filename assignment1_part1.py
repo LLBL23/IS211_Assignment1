8967a912aa4e2bbc8fdb3fe580aaef7bb9af9729
LLBL23 <ltbowenslarson@gmail.com>
2024-01-29 21:21:43 -0500

    commit my changes to list_divide

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,7 +1,14 @@
-
-def list_divide(numbers, divide):
+# -*- coding: utf-8 -*-
+def list_divide(numbers, divide=2):
     """
     The function returns the number of elements in the numbers list that are divisibleby divide
+    """
+    items = [x for x in numbers if x % divide == 0]
+    return len(items)
+
+class ListDivideException(Exception):
+    """
+    Raise List Divide Exception
     """
     pass
 
@@ -9,11 +16,18 @@ def test_list_divide():
     """
     Test listDivide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    listDivide = [([1,2,3,4,5]), 2, 2,
+                  ([2,4,6,8,10]),2, 5,
+                  ([30, 54, 63,98, 100], 10), 2,
+                  ([]), 2, 0,
+                  ([1,2,3,4,5], 1), 2, 5]
+
+    for nums, div, res in listDivide:
+        comp = list_divide(nums, div)
+        if comp != res:
+            raise ListDivideException()
+
+
+
 if __name__ == "__main__":
-    testListDivide()
+    print (test_list_divide())
